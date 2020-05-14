ab0652fb4530fcca716f18e49d70ff951c0e113c
AshleyQSmith <smittyq@gmail.com>
2020-05-13 20:25:06 -0600

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consolegame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
